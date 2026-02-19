10c38dc912474d6c10796b8774cb63b7a1549e43
Boris Savushkin <142305407+Reelinq@users.noreply.github.com>
2026-02-19 13:40:06 +0200

    Rename ensure_directories to _ensure_directories for consistency and update references

--- src/config/main.py
+++ src/config/main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from src.config.cli_args import get_cli_args
 from src.config.cli_options import build_cli_options
-from src.config.paths import ensure_directories
+from src.config.paths import _ensure_directories
 
 
 @dataclass
@@ -14,31 +14,31 @@ class Config:
     cli_options: dict = None
 
     def __post_init__(self) -> None:
-        ensure_directories(self.output_folder, self.logs_folder)
+        _ensure_directories(self.output_folder, self.logs_folder)
 
     @classmethod
     def initialize_config(cls) -> None:
         args = get_cli_args()
         cls.cli_options = build_cli_options(args)
-        cls.json_path = cls.get_memories_json_path()
-        cls.output_folder = cls.get_output_folder()
-        cls.logs_folder = cls.get_logs_folder()
+        cls.json_path = cls._get_memories_json_path()
+        cls.output_folder = cls._get_output_folder()
+        cls.logs_folder = cls._get_logs_folder()
         cls._ensure_directories()
 
     @classmethod
-    def get_memories_json_path(cls) -> Path:
+    def _get_memories_json_path(cls) -> Path:
         if cls.cli_options["memories_json"]:
             return Path(cls.cli_options["memories_json"])
         return Path("data/memories_history.json")
 
     @classmethod
-    def get_output_folder(cls) -> Path:
+    def _get_output_folder(cls) -> Path:
         if cls.cli_options["output"]:
             return Path(cls.cli_options["output"])
         return Path("downloads")
 
     @classmethod
-    def get_logs_folder(cls) -> Path:
+    def _get_logs_folder(cls) -> Path:
         if cls.cli_options["logs_path"]:
             return Path(cls.cli_options["logs_path"])
         return Path("logs")
