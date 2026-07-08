bc5a5658d38bdf95fbaef78ed8eab5b697d294a4
Boris Savushkin <142305407+Reelinq@users.noreply.github.com>
2026-07-08 15:30:33 +0300

    fix: remove stale ZipProcessor export

--- src/media_dispatcher/__init__.py
+++ src/media_dispatcher/__init__.py
@@ -1,6 +1,5 @@
 from src.media_dispatcher.image_processor import process_image
 from src.media_dispatcher.media_dispatcher import process_media
 from src.media_dispatcher.video_processor import ProcessVideo
-from src.media_dispatcher.zip_processor import ZipProcessor
 
-__all__ = ["ProcessVideo", "ZipProcessor", "process_image", "process_media"]
+__all__ = ["ProcessVideo", "process_image", "process_media"]
